277c15bc71067741690fb36dff8501ffed26a3f4
MHFLCMAY2UCSD <94334792+MHFLCMAY2UCSD@users.noreply.github.com>
2022-03-22 20:19:17 -0700

    major update
    quotes changes now...

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 console.log("Connected!");
+
+//Constant changing header
+var quotes = [
+			 "The future is now, old man!", 
+			 "The way to get started is to quit talking and begin doing.",
+			 "One does not simply walk in to...",
+			 "Get in loser we're going to shopping!"
+			 ];
+
+var authors = [
+				"Dewey Malcom from Malcolm in the Middle", 
+				"Walt Disney", 
+				"Eddard Stark", 
+				"Regina George"
+			  ];
+
+//Getting the quotes
+var x = document.querySelector("#quotes");
+
+//Getting the people or person who said that quote
+var y = document.querySelector("#authors")
+
+//Get the length of the current array
+var length = quotes.length;
+var randInt = 0;	//For changing quotes/authors
+var sleepTime = 0;	//For Enjoying and respecting the peace
+
+//Random number generator (Python > JS)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//Python sleep() == this func.
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//Constant Rotation
+async function polychange() {
+    while(true){
+    	console.log(x.textContent);
+		console.log(y.textContent);
+		var dDip = randInt;
+		randInt = getRandomInt(0, length);
+
+		//Want to prevent the repeat of quotes consecutively.
+		while(dDip == randInt){
+			randInt = getRandomInt(0, length);
+		}
+
+		//Create or change the quotes
+		x.textContent = quotes[randInt];
+		y.textContent = "-" + authors[randInt];
+
+		sleepTime = getRandomInt(3, 7);
+		console.log(sleepTime * 1000);
+        await sleep(sleepTime * 1000);
+
+    }
+}
+
+function main(){
+	polychange();
+}
+
+main();
